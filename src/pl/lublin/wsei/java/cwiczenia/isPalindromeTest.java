ed2ce3011a5ad89990097a4ceb603cd50e935273
35167 <35167@studentwsei.pl>
2021-03-05 10:26:48 +0100

    Is Palindrome

---
+++ src/pl/lublin/wsei/java/cwiczenia/isPalindromeTest.java
@@ -0,0 +1,18 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+import pl.lublin.wsei.java.cwiczenia.mylib.StringFun;
+import java.util.Scanner;
+
+public class isPalindromeTest {
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        do{
+            System.out.println("Podaj tekst do sprawdzenia");
+            String tekst = scn.next();
+            if (tekst.equals("0")) break;
+            System.out.print("Tekst: " + tekst + (StringFun.isPalindrome(tekst) ? "" : "nie ")+ "jest palindromem");
+        }while (true);
+    }
+}
---
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/StringFun.java
@@ -0,0 +1,9 @@
+package pl.lublin.wsei.java.cwiczenia.mylib;
+
+import pl.lublin.wsei.java.cwiczenia.isPalindromeTest;
+
+public class StringFun {
+    public static boolean isPalindrome(String word){
+        return true;
+    }
+}
